23a751073103487ec6340494499ae086a8a86ce6
Manu Cristini <manu.cristini@epfl.ch>
2023-10-13 17:09:34 +0200

    added cs buttons to decoder

--- template/vhdl/decoder.vhd
+++ template/vhdl/decoder.vhd
@@ -7,7 +7,8 @@ entity decoder is
         address : in  std_logic_vector(15 downto 0);
         cs_LEDS : out std_logic;
         cs_RAM  : out std_logic;
-        cs_ROM  : out std_logic
+        cs_ROM  : out std_logic;
+        cs_buttons : out std_logic;
     );
 end decoder;
 
@@ -24,5 +25,6 @@ begin
     cs_ROM  <= '1' when s_address <= 16#0FFC# else '0';
     cs_RAM  <= '1' when (s_address >= 16#1000#) and (s_address <= 16#1FFC#) else '0';
     cs_LEDS <= '1' when (s_address >= 16#2000#) and (s_address <= 16#200C#) else '0';
+    cs_buttons <= '1' when (s_address >= 16#2030#) and (s_address <= 16#2034#) else '0';
     
 end synth;
